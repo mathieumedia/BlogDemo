b5380680f8e381cc748cafb3515e9801c9fbd0be
Guy Mathieu <57466994+GuyEMathieu@users.noreply.github.com>
2022-04-30 15:56:20 -0400

    init backend files

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const colors = require('colors');
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 5000;
+const dotenv = require('dotenv').config();
+
+
+const app = express();
+
+app.use(express.json({extended: false}));
+
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`.green.underline.bold);
+    }catch(err){
+        console.log(`ERROR: ${err.message}`.bgRed.underline.bold);
+        process.exit(1);
+    }
+}
+
+connectDB();
+
+app.listen(PORT, () => console.info(`Server is running on port ${PORT}`.green.underline.bold));
